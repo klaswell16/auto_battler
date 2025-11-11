3a8f60f6799920d60ca9443a754016474d785f6b
klaswell16 <kcclaswell@gmail.com>
2025-11-11 12:44:38 -0500

    swap atk to pwr in main scene

--- scripts/battle_scene.gd
+++ scripts/battle_scene.gd
@@ -6,15 +6,15 @@ extends Control
 var champion_scene: PackedScene = preload("res://scenes/Champion.tscn")
 
 var player_team := [
-	{"name":"Shieldbearer","hp":40,"atk":8,"armor":3},
-	{"name":"Archer","hp":25,"atk":12,"armor":1},
-	{"name":"Priest","hp":30,"atk":4,"armor":1}
+	{"name":"Shieldbearer","hp":40,"pwr":8,"armor":3},
+	{"name":"Archer","hp":25,"pwr":12,"armor":1},
+	{"name":"Priest","hp":30,"pwr":4,"armor":1}
 ]
 
 var enemy_team := [
-	{"name":"Bandit","hp":28,"atk":10,"armor":1},
-	{"name":"Sorcerer","hp":22,"atk":14,"armor":0},
-	{"name":"Brute","hp":50,"atk":6,"armor":4}
+	{"name":"Bandit","hp":28,"pwr":10,"armor":1},
+	{"name":"Wizard","hp":22,"pwr":14,"armor":0},
+	{"name":"Brute","hp":50,"pwr":6,"armor":4}
 ]
 
 func _ready() -> void:
